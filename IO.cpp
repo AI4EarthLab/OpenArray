e57c4c23009554ec78a74dd536dffa6f5a171544
sxzhang2009 <sxzhang2009@gmail.com>
2017-10-23 13:28:10 +0800

    xx

--- IO.cpp
+++ IO.cpp
@@ -1,11 +1,14 @@
 
 #include "IO.hpp"
+#include "Utils.hpp"
 
 namespace oa{
   namespace io{
     void save(const ArrayPtr& A,
 	      const std::string& filename,
 	      const std::string& varname){
+      
+      oa::utils::mpi_datatype(A.data_type());
       
     };
 
